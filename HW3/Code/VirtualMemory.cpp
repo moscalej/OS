b8ac40a1c88212c767b500cbb4a3c1f4911e2497
Alejandro <moscalej@gmail.com>
2017-06-20 20:22:58 +0300

    <START> first step on our 5 days project hope to finish in 3

---
+++ HW3/Code/VirtualMemory.cpp
@@ -0,0 +1,7 @@
+//
+// Created by alejandro on 6/20/2017.
+//
+
+#include "VirtualMemory.h"
+
+VirtualMemory::VirtualMemory() {}
---
+++ HW3/Code/VirtualMemory.h
@@ -0,0 +1,16 @@
+//
+// Created by alejandro on 6/20/2017.
+//
+
+#ifndef HW3_VIRTUALMEMORY_H
+#define HW3_VIRTUALMEMORY_H
+
+
+class VirtualMemory {
+public:
+    VirtualMemory();
+
+};
+
+
+#endif //HW3_VIRTUALMEMORY_H
