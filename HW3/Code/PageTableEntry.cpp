f821b083253bb7c3aac563036b8b285523a73e00
chaimovy <chaimovy@gmail.com>
2017-06-23 15:59:44 +0300

    PT done

--- HW3/Code/PageTableEntry.cpp
+++ HW3/Code/PageTableEntry.cpp
@@ -5,17 +5,23 @@
 #include "PageTableEntry.h"
 
 int PageTableEntry::get_page_address() {
-    return nullptr;
+    return address_;
 }
 
 void PageTableEntry::set_page_address(int adr) {
+    address_=adr;
 
 }
 
 bool PageTableEntry::is_valid() {
-    return false;
+    return valid_;
 }
 
 void PageTableEntry::set_valid(bool valid) {
+    valid_=valid
 
+}
+
+PageTableEntry::PageTableEntry() {
+    valid_=false;
 }
