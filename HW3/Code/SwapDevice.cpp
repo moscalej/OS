b4c8392c6145e0d1fe02d2f07a74d0fc8e9f0447
Alejandro <moscalej@gmail.com>
2017-06-23 16:23:37 +0300

    structure finish we need to set methods on the interfaces

--- HW3/Code/SwapDevice.cpp
+++ HW3/Code/SwapDevice.cpp
@@ -1,4 +1,6 @@
 #include "SwapDevice.h"
+#include "HelpFunc.h"
+
 #define FRAMESZ 4096
 
 using namespace std;
@@ -22,6 +24,12 @@ int SwapDevice::ReadFrameFromSwapDevice(int pageNumber, int* pageIn) {
 }
 
 SwapDevice::SwapDevice(PhysMem *PM) : PM(PM) {
+    /*
+     * We only fill the queue with 64 values that will rotate the number of frames
+     */
+    for (int i = 0; i < 64; ++i) {
+       this->freeFramesList.push(std::make_pair(i,i));
+    }
 
 }
 
@@ -29,5 +37,22 @@ int SwapDevice::write_this_page_to_the_frame(unsigned virtual_address) {
     /*
      * get VM, pop frame_number from queue, swap physical memory, return frame_number,
      * push back to queue
+     * we will work here only with pages number for that we need to change the values to page numbers from virtual address
+     *
      */
+
+    int page_number = bits_to_take(12,22,virtual_address);
+    int old_page_number = this->freeFramesList.front().first;
+    int frame_number = this->freeFramesList.front().second;
+
+    int * frame_pointer = this->PM->GetFrame(frame_number);
+    this->WriteFrameToSwapDevice(old_page_number, frame_pointer);
+    int result= this->ReadFrameFromSwapDevice(page_number,frame_pointer);
+    this->freeFramesList.pop();
+    std::pair<int,int> temp = std::make_pair(page_number,frame_number);
+    this->freeFramesList.push(temp);
+
+    return (result==0)?frame_number:-1;
+
+
 }
--- HW3/Code/SwapDevice.h
+++ HW3/Code/SwapDevice.h
@@ -16,10 +16,10 @@ public:
 
 
 private:
-    void WriteFrameToSwapDevice(int pageNumber, int t); //Write the content of page to the swap device, "pageOut" is the frame base pointer where the page is now allocated
+    void WriteFrameToSwapDevice(int pageNumber, int * page_out); //Write the content of page to the swap device, "pageOut" is the frame base pointer where the page is now allocated
     int ReadFrameFromSwapDevice(int pageNumber, int* pageIn); //Put the content of the page in "page". " pageIn " is the frame base pointer to where the page is about to be allocated, returns -1 if page is not stored in the swap device.
     PhysMem * PM;
     std::unordered_map<int, int*> _data;
-    std::queue<int*> freeFramesList;
+    std::queue< std::pair<int,int> > freeFramesList;
     size_t _size;
 };
